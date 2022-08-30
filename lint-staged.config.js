a7791d8f7476862213bd359ea368820ba19c099f
Zach Kirsch <zachkirsch@gmail.com>
2022-08-30 11:18:08 -0700

    Don't run organize-imports in lint-staged (too slow)

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,11 +1,6 @@
 module.exports = {
-    "**/*.js{,x}": ["yarn organize-imports", "yarn format"],
-    "**/*.ts{,x}": [
-        "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
-        "yarn organize-imports",
-        "yarn format",
-    ],
-    "**/*.{json,yml,html,css,less,scss,md}": "yarn format",
+    "**/*.ts{,x}": ["eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js", "yarn format"],
+    "**/*.{js,json,yml,html,css,less,scss,md}": "yarn format",
     "**/{*,__}": () => "yarn lint:monorepo",
     "**/{package.json, __}": () => "yarn install --immutable",
 };
