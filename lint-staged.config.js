40981d406bc94b1f6315b15e727d2f4514c4a81c
Zach Kirsch <zachkirsch@gmail.com>
2022-08-30 10:59:34 -0700

    Only run organize imports on ts files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    "**/*.{ts,tsx}": "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
-    "**/{*,_}": "yarn format",
+    "**/*.{ts,tsx}": [
+        "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
+        "yarn organize-imports",
+        "yarn format",
+    ],
     "**/{*,__}": () => "yarn lint:monorepo",
     "**/{package.json, __}": () => "yarn install --immutable",
 };
--- packages/declaration-handler/src/DeclarationHandler.ts
+++ packages/declaration-handler/src/DeclarationHandler.ts
@@ -1,11 +1,11 @@
-import { GeneratorContext } from "./GeneratorContext";
 import { File } from "./File";
+import { GeneratorContext } from "./GeneratorContext";
 
 export interface DeclarationHandler<T> {
     run: (declaration: T, args: DeclarationHandlerArgs) => void | Promise<void>;
 }
 
 export interface DeclarationHandlerArgs {
-    withFile: (run: (file: File) => void | Promise<void>) => Promise<void>;
     context: GeneratorContext;
+    withFile: (run: (file: File) => void | Promise<void>) => Promise<void>;
 }
