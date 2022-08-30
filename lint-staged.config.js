8aab08cd6c35d9c574250a958d4faaa9cf3d2e10
Zach Kirsch <zachkirsch@gmail.com>
2022-08-30 11:02:51 -0700

    Add to circle

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-    "**/*.{ts,tsx}": [
+    "**/*.[jt]sx?": [
         "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
         "yarn organize-imports",
         "yarn format",
     ],
+    "**/*.{json,yml,html,css,less,scss,md}": "yarn format",
     "**/{*,__}": () => "yarn lint:monorepo",
     "**/{package.json, __}": () => "yarn install --immutable",
 };
