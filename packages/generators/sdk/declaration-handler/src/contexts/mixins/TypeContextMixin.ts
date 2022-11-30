9601b029fb85427a595e984a2581d2257ebcf7fa
Zach Kirsch <zachkirsch@gmail.com>
2022-11-29 23:57:00 -0500

    Fix eslint

--- packages/generators/sdk/declaration-handler/src/contexts/mixins/TypeContextMixin.ts
+++ packages/generators/sdk/declaration-handler/src/contexts/mixins/TypeContextMixin.ts
@@ -2,14 +2,13 @@ import { DeclaredTypeName, ResolvedTypeReference, TypeReference } from "@fern-fe
 import { TypeReferenceNode } from "@fern-typescript/commons-v2";
 import { GeneratedType } from "../../generated-types";
 import { Reference } from "../../Reference";
-import { TypeContext } from "../TypeContext";
 
 export interface TypeContextMixin {
     getReferenceToType: (typeReference: TypeReference) => TypeReferenceNode;
     getReferenceToNamedType: (typeName: DeclaredTypeName) => Reference;
     resolveTypeReference: (typeReference: TypeReference) => ResolvedTypeReference;
     resolveTypeName: (typeName: DeclaredTypeName) => ResolvedTypeReference;
-    getGeneratedType: (typeName: DeclaredTypeName) => GeneratedType<TypeContext>;
+    getGeneratedType: (typeName: DeclaredTypeName) => GeneratedType;
 }
 
 export interface WithTypeContextMixin {
